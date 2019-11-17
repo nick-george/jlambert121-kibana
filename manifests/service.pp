cbcafe25dc4299acd8438a086d68e52a3ecd8a87
Nick George <7453754+nick-george@users.noreply.github.com>
2019-11-18 09:40:08 +1100

    syntax

--- manifests/service.pp
+++ manifests/service.pp
@@ -13,7 +13,7 @@ class kibana::service {
       provider => $::kibana::params::service_provider,
     }
   }
-  elsif $package_provider == 'docker' {
+  elsif $::kibana::package_provider == 'docker' {
   #docker run -it --name gerald --mount type=bind,source=/etc/countersight,destination=/etc/countersight,readonly --mount type=bind,source=/data/configs,destination=/data/configs --mount type=bind,source=/etc/kibana,destination=/etc/kibana,readonly --mount type=bind,source=/etc/kibana/kibana.yml,destination=/usr/share/kibana/config/kibana.yml,readonly -p 443:443 registry.countersight.co:5000/countersight_kibana:7.4.2-20191114008
     docker::run { 'kibana':
       image            => 'registry.countersight.co:5000/countersight_kibana:7.4.2-20191114008',
@@ -24,5 +24,7 @@ class kibana::service {
                             '--mount type=bind,source=/etc/kibana,destination=/etc/kibana,readonly',
                             '--mount type=bind,source=/etc/kibana/kibana.yml,destination=/usr/share/kibana/config/kibana.yml,readonly']
     }
+  } else {
+    fail("kibana package provider must be 'docker' or 'rpm'")
   }
 }
