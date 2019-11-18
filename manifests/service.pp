96cbbdc1f92cdf7ed7ec904ece010cc4ec79ee15
Nick George <7453754+nick-george@users.noreply.github.com>
2019-11-19 10:54:42 +1100

    Fixing up the kibana version

--- manifests/service.pp
+++ manifests/service.pp
@@ -16,9 +16,9 @@ class kibana::service {
   elsif $::kibana::package_provider == 'docker' {
   #docker run -it --name gerald --mount type=bind,source=/etc/countersight,destination=/etc/countersight,readonly --mount type=bind,source=/data/configs,destination=/data/configs --mount type=bind,source=/etc/kibana,destination=/etc/kibana,readonly --mount type=bind,source=/etc/kibana/kibana.yml,destination=/usr/share/kibana/config/kibana.yml,readonly -p 443:443 registry.countersight.co:5000/countersight_kibana:7.4.2-20191114008
     docker::run { 'kibana':
-      image            => 'registry.countersight.co:5000/countersight_kibana:latest',
+      image            => "registry.countersight.co:5000/countersight_kibana:${::kibana::version}",
       ports            => ['443:5601'],
-      extra_parameters => [ '--restart=always',
+      extra_parameters => [ '--restart=unless-stopped',
                             '--mount type=bind,source=/etc/countersight,destination=/etc/countersight,readonly',
                             '--mount type=bind,source=/data/configs,destination=/data/configs',
                             '--mount type=bind,source=/etc/kibana,destination=/etc/kibana,readonly',
