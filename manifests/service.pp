378f7b6a8273391524f6c72c17a2cd67ab3a258e
Nick George <7453754+nick-george@users.noreply.github.com>
2019-11-19 08:42:25 +1100

    Changing kibana port to 5601

--- manifests/service.pp
+++ manifests/service.pp
@@ -17,7 +17,7 @@ class kibana::service {
   #docker run -it --name gerald --mount type=bind,source=/etc/countersight,destination=/etc/countersight,readonly --mount type=bind,source=/data/configs,destination=/data/configs --mount type=bind,source=/etc/kibana,destination=/etc/kibana,readonly --mount type=bind,source=/etc/kibana/kibana.yml,destination=/usr/share/kibana/config/kibana.yml,readonly -p 443:443 registry.countersight.co:5000/countersight_kibana:7.4.2-20191114008
     docker::run { 'kibana':
       image            => 'registry.countersight.co:5000/countersight_kibana:7.4.2-20191114008',
-      ports            => ['443:443'],
+      ports            => ['443:5601'],
       extra_parameters => [ '--restart=always',
                             '--mount type=bind,source=/etc/countersight,destination=/etc/countersight,readonly',
                             '--mount type=bind,source=/data/configs,destination=/data/configs',
